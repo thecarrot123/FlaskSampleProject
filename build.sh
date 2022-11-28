1126eab5600dc4181757a9e02b62bf4e6b517fff
thecarrot123 <theknife2001.al@gmail.com>
2022-11-29 02:05:02 +0300

    edit build.sh

--- build.sh
+++ build.sh
@@ -3,30 +3,28 @@ set -e
 
 green="\e[32m"
 red="\e[31m"
-black="\e[0m"
+default="\e[0m"
 blue="\e[94m"
 
 
 create_init() {
-    cd $1
     [ -f __init__.py ] && rm __init__.py
-    files=$(ls | grep -E "*\.py")
+    files=$(ls | grep -E "*\.py" | tr " " "\n")
+    [ ! ${#files[0]} -gt 0 ] && echo -e "- ${red}$1 file is empty!${default}" && return 0
+
     touch '__init__.py'
-    for file in $files; do
-        echo -e "${red}- ${black}Importing $file to $1"
+    echo -e "${blue}*${default} creating __init__.py for $1"
+    folders+=("$1")
+    for file in ${files[@]}; do
+        
+        echo -e "${red}- ${default}Importing $file to $1"
         echo "from main.$1.${file::-3} import *" | tee -a __init__.py >> /dev/null
     done
-    cd ..
-    echo -e "${green}+ ${black}done!\n"
+    echo -e "${green}+ ${default}done!\n"
 }
 
 create_init_for_dir() {
-    if [ -d $1 ]
-    then
-        echo -e "${blue}*${black} creating __init__.py for $1"
-        create_init $1
-        folders+=("$1")
-    fi
+    [ -d $1 ] && cd $1 && create_init $1 && cd ..
 }
 
 [ ! -d main ] && echo "<main> directory not found." && exit
@@ -34,4 +32,4 @@ cd main
 create_init_for_dir routes
 create_init_for_dir models
 create_init_for_dir forms
-echo -e "created __init__.py files for: ${blue}${folders[@]}${black}"
+echo -e "created __init__.py files for: ${blue}${folders[@]}${default}"
